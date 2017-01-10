49fbd3d3e51a200dce45eb053380f8cc712f2ea7
Nattawut Vejkanchana aka Schubert S <tongpython@gmail.com>
2017-01-10 08:42:01 +0700

    Update convert_excel_to_csv_pd.py

--- convert_excel_to_csv_pd.py
+++ convert_excel_to_csv_pd.py
@@ -11,7 +11,7 @@ def excel_to_csv(excel_name,csv_name,sheetname='Sheet1'):
                     ,index=False
                     ,date_format='%Y-%m-%d')
 
-folder_name = "C:/Users/nattawut vejkanchana/Documents/work/Projects/Kubota/ascore2/"
+folder_name = "C:/yourfolder/"
 
 ##excel_name = folder_name+"main_good_rows.xlsx"
 ##csv_name = folder_name+"main_good_rows.csv"
@@ -23,8 +23,9 @@ folder_name = "C:/Users/nattawut vejkanchana/Documents/work/Projects/Kubota/asco
 ##
 ##excel_to_csv(excel_name,csv_name)
 
+#convert many files in a folder
 #l_name = ["main_fix_type.xlsx","main_fix_type2.xlsx","main_fix_type3.xlsx","main_fix_type4.xlsx"]
-l_name = ["a_scorce_acc_level.xlsx"]
+l_name = ["yourfile.xlsx"]
 for excel_name in l_name:
     excel_name = folder_name+excel_name
     csv_name = excel_name[ 0 : excel_name.find(".") ] + ".csv"
